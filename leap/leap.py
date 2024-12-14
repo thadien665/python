1d941f8f56e5754e1b0973c6014a58c9c7747a33
thadien <marcinlejman665@gmail.com>
2024-12-14 16:24:49 +0100

    tests ok

---
+++ leap/leap.py
@@ -0,0 +1,7 @@
+def leap_year(year):
+    if year % 100 == 0 and year % 400 == 0:
+        return True
+    elif year % 100 != 0 and year % 4 == 0:
+        return True
+    else:
+        return False
